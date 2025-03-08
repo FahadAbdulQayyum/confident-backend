a08a1562f822d9f4a4ed6fff871816ab19a46093
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-08 23:07:17 +0500

    added eid-ul-adha api

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/api/eid', (req, res) => {
     res.send('Welcome to \'Eid\'s page!');
 })
 
+app.get('/api/eid-ul-azha', (req, res) => {
+    res.send('Welcome to Baqra Eid, where is your Goat, man?');
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
